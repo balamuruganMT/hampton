cfb3141ee82d030cb7adbf41b1f4b1c7d26ca87f
asuresh86 <asuresh.ui@gmail.com>
2020-05-14 14:52:08 +0530

    Initial Commit

--- src/Components/ImageAnalysis/ImageAnalysis.js
+++ src/Components/ImageAnalysis/ImageAnalysis.js
@@ -1,6 +1,10 @@
 import React,{Component} from 'react';
 import './ImageAnalysis.css';
 
+import Logo from '../../images/logo.png';
+import Upload from '../../images/upload.svg';
+import Profile from '../../images/profile.svg';
+
 class ImageAnalysis extends Component{
     constructor(props){
         super(props);
@@ -10,8 +14,48 @@ class ImageAnalysis extends Component{
     }
     render(){
         return(
-            <div>
-                <h1>sdfsdfsd</h1>
+            <div className="mainWrapper">
+                <div className="header">
+                    <div className="container">
+                        <div className="d-flex">
+                            <div className="logo">
+                                <img src={Logo} alt="Logo" height="36"/>
+                            </div>
+                            <div className="navRight">
+                                <div className="profile">
+                                    <img src={Profile} alt="Profile" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="mainContainer">
+                    <div className="container">
+                        <h3>Image analysis</h3>
+                        <div className="imgAnalysis">
+                            <div className="iaRow">
+                                <div className="box borderedBox">
+                                    <div>
+                                    <img src={Upload} width="40" alt="upload"/>
+                                    <p>Paste / Upload Your Job Description Here!</p>
+                                    <span className="or">Or</span>
+                                    <div className="browse"><span>BROWSE</span><input type="file" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="iaRow">
+                                <div className="box skillBox">
+                                    <p>Resulted Skills</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="btnGroup">
+                            <button className="btns solidBtn">SUBMIT</button>
+                            <button className="btns dashedBtn">RESET</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
