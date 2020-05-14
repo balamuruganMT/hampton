deac452679dbfeb73d747c5697c373f2c705b549
Balamurugan Rajendran <balamurugan.r@mitosistech.com>
2020-05-14 15:38:13 +0530

    [update]added dropzone for image upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ImageAnalysis from './Components/ImageAnalysis/ImageAnalysis';
 
--- src/Components/ImageAnalysis/ImageAnalysis.js
+++ src/Components/ImageAnalysis/ImageAnalysis.js
@@ -1,26 +1,27 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './ImageAnalysis.css';
 
 import Logo from '../../images/logo.png';
 import Upload from '../../images/upload.svg';
 import Profile from '../../images/profile.svg';
 import Banner from '../../images/banner.jpg';
+import Dropzone from 'react-dropzone';
 
-class ImageAnalysis extends Component{
-    constructor(props){
+class ImageAnalysis extends Component {
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
 
         }
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="mainWrapper">
                 <div className="header">
                     <div className="container">
                         <div className="d-flex">
                             <div className="logo">
-                                <img src={Logo} alt="Logo" height="36"/>
+                                <img src={Logo} alt="Logo" height="36" />
                             </div>
                             <div className="navRight">
                                 <div className="profile">
@@ -43,12 +44,25 @@ class ImageAnalysis extends Component{
                                     <span className="or">Or</span>
                                     <div className="browse"><span>BROWSE</span><input type="file" /></div>
                                     </div> */}
-                                    <img src={Banner} className="bannerImg" alt="Banner"/>
+                                    {/* <img src={Banner} className="bannerImg" alt="Banner" /> */}
+                                    <Dropzone multiple={false} accept="image/*" onDrop={acceptedFiles => this.handleDropImage(acceptedFiles)}>
+                                        {({ getRootProps, getInputProps, isDragReject, rejectedFiles }) => (
+                                            <section>
+                                                <div className="dropzone_content" {...getRootProps()}>
+                                                    <input {...getInputProps()} />
+                                                    <img src={Upload} width="40" alt="upload" />
+                                                    <p>Paste / Upload Your Image Here!</p>
+                                                    <span className="or">Or</span>
+                                                    <div className="browse"><span>BROWSE</span></div>
+                                                </div>
+                                            </section>
+                                        )}
+                                    </Dropzone>
                                 </div>
                             </div>
                             <div className="iaRow">
                                 <div className="box skillBox">
-                                    <p>Resulted Skills</p>
+                                    <p>Resulted Output</p>
                                 </div>
                             </div>
                         </div>
