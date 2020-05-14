b6ac0b738af5fdf875f262247db4d63eb21cb045
Balamurugan Rajendran <balamurugan.r@mitosistech.com>
2020-05-14 12:11:23 +0530

    [added]Image analysis component added

---
+++ src/Components/ImageAnalysis/ImageAnalysis.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import './ImageAnalysis.css';
+
+class ImageAnalysis extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h1>sdfsdfsd</h1>
+            </div>
+        )
+    }
+}
+
+export default ImageAnalysis;
